daafef0ed36c6514f1e41949d996c27714300ae6
nwillhoft <70575561+nwillhoft@users.noreply.github.com>
2023-05-09 11:36:49 +0100

    Version bump for Release 111

--- sql/tables.sql
+++ sql/tables.sql
@@ -30,11 +30,11 @@ CREATE TABLE `meta` (
 # Add schema type and schema version to the meta table
 INSERT INTO meta (meta_key, meta_value) VALUES
   ('schema_type', 'ontology'),
-  ('schema_version', '110');
+  ('schema_version', '111');
 
 # Patches included in this schema file
 INSERT INTO meta (meta_key, meta_value)
-  VALUES ('patch', 'patch_109_110_a.sql|schema_version');
+  VALUES ('patch', 'patch_110_111_a.sql|schema_version');
 
 CREATE TABLE `ontology` (
   `ontology_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
